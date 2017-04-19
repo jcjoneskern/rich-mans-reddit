e718d2a7264c047535ade36e5ee0937e746b3270
J-C <jcjoneskern@gmail.com>
2017-04-18 23:15:51 -0400

    refactored

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var app = angular.module("redditMod", []);
+(function() {
+  var app = angular.module("redditMod", []);
 
-app.controller('redditCtrl', ['$scope', '$http', function($scope, $http) {
+  app.controller('redditCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $http.get('https://www.reddit.com/r/aww.json').then(function(response) {
-    var redditData = response.data.data.children;
-    console.log(redditData);
-    $scope.posts = redditData;
-  });
-
-
-}]);
+    $http.get('https://www.reddit.com/r/aww.json').then(function(response) {
+      $scope.posts = response.data.data.children;
+    });
+  }]);
+})();
